82de453069612d49cf2d12c8c8eccfe0e943072b
yuliaslatvitskaya <yuli.slat@yandex.ru>
2023-07-25 12:15:45 +0200

    feat: new main

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,9 @@
 import { Button } from './components/ui/button/button.tsx'
-import { Card } from './components/ui/card'
-import { Checkbox } from './components/ui/checkbox/checkbox.tsx'
 
 export function App() {
   return (
     <div>
       <Button />
-      <Card />
-      <Checkbox />
     </div>
   )
 }
--- src/components/ui/card/card.tsx
+++
@@ -1,8 +0,0 @@
-import s from './card.module.scss'
-export const Card = () => {
-  return (
-    <div className={s.cardWrapper}>
-      <h1>Test Card</h1>
-    </div>
-  )
-}
