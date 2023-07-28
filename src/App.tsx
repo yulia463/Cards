e3a675a2d23775596e80fd9a9baff53bf84c06a0
yuliaslatvitskaya <yuli.slat@yandex.ru>
2023-07-28 16:29:26 +0200

    fix: revert App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,3 @@
-import { useState } from 'react'
-
 import {
   createBrowserRouter,
   createRoutesFromElements,
@@ -7,41 +5,18 @@ import {
   RouterProvider,
 } from 'react-router-dom'
 
-import Dropdown from './components/ui/dropdown/dropdown.tsx'
+
 import Layout from './components/ui/Layout/Layout.tsx'
-import { Select } from './components/ui/select/Select.tsx'
 import Login from './pages/LoginPage/Login.tsx'
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<Layout />}>
-      <Route path={'/login'} element={<Login />} />
+      <Route path={'/login'} element={<Login />}/>
     </Route>
   )
 )
 
 export function App() {
-  const [isDropdownOpened, setIsDropdownOpened] = useState(false)
-  const options = [<div key={'1'}>Hell0!</div>, <div key={'2'}>Bye</div>]
-  const onClickHandler = () => {
-    setIsDropdownOpened(true)
-  }
-
-  return (
-    <div>
-      <RouterProvider router={router} />
-      <div>
-        <button
-          onBlur={() => {
-            setIsDropdownOpened(false)
-          }}
-          onClick={onClickHandler}
-        >
-          pupa
-        </button>
-        <Dropdown isDropdownOpen={isDropdownOpened} options={options} />
-      </div>
-      <Select />
-    </div>
-  )
+  return <RouterProvider router={router} />
 }
