db3493aeec22649e2df2b6f35952f34a726457a5
yuliaslatvitskaya <yuli.slat@yandex.ru>
2023-07-31 15:39:37 +0200

    feat: add singIn

--- src/App.tsx
+++ src/App.tsx
@@ -5,18 +5,30 @@ import {
   RouterProvider,
 } from 'react-router-dom'
 
+import { SignIn } from '../src/components/auth/signIn'
 
+import { LoginForm } from './components/auth/login-form/loginForm.tsx'
 import Layout from './components/ui/Layout/Layout.tsx'
 import Login from './pages/LoginPage/Login.tsx'
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<Layout />}>
-      <Route path={'/login'} element={<Login />}/>
+      <Route path={'/login'} element={<Login />} />
     </Route>
   )
 )
 
 export function App() {
-  return <RouterProvider router={router} />
+  return (
+    <div>
+      <RouterProvider router={router} />
+      <LoginForm />
+      <SignIn
+        onSubmit={() => {
+          'test'
+        }}
+      />
+    </div>
+  )
 }
