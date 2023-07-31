f743f569fb4336f8e3af7cadb3d1aff269e0d70c
KarinaFomina04 <karinafomina.19970804@gmail.com>
2023-07-31 13:39:14 +0200

    feat: refactor textField(Input)

--- src/components/ui/textField/textField.tsx
+++ src/components/ui/textField/textField.tsx
@@ -1,10 +1,4 @@
-import {
-  ChangeEvent,
-  DetailedHTMLProps,
-  InputHTMLAttributes,
-  KeyboardEvent,
-  ReactNode,
-} from 'react'
+import {DetailedHTMLProps, FC, InputHTMLAttributes, ReactNode} from 'react'
 
 import s from './textField.module.scss'
 
@@ -18,53 +12,43 @@ type SupertextFieldPropsType = Omit<DefaultInputPropsType, 'type'> & {
   onEnter?: () => void
   error?: ReactNode
   spanClassName?: string
-  disabled?: true
+  disabled?: boolean
   label?: string
+  errorMessage?: any
 }
 
-export const TextField: React.FC<SupertextFieldPropsType> = ({
-  onChange,
+export const TextField: FC<SupertextFieldPropsType> = ({
   onChangeText,
-  onKeyPress,
   onEnter,
   error,
-  className,
   label,
   spanClassName,
-  id,
+  errorMessage,
+  disabled,
 
   ...restProps
 }) => {
-  const onChangeCallback = (e: ChangeEvent<HTMLInputElement>) => {
-    onChange?.(e)
+  // const onChangeCallback = (e: ChangeEvent<HTMLInputElement>) => {
+  //   onChange?.(e)
+  //
+  //   onChangeText?.(e.currentTarget.value)
+  // }
+  // const onKeyPressCallback = (e: KeyboardEvent<HTMLInputElement>) => {
+  //   onKeyPress?.(e)
+  //
+  //   onEnter && e.key === 'Enter' && onEnter()
+  // }
 
-    onChangeText?.(e.currentTarget.value)
-  }
-  const onKeyPressCallback = (e: KeyboardEvent<HTMLInputElement>) => {
-    onKeyPress?.(e)
-
-    onEnter && e.key === 'Enter' && onEnter()
-  }
-
-  const finalSpanClassName = s.error + (spanClassName ? ' ' + spanClassName : '')
-  const finalInputClassName =
-    s.input +
-    (error ? ' ' + s.errorInput : ' ' + s.superInput) +
-    (className ? ' ' + s.className : '')
+  // const finalSpanClassName = s.error + (spanClassName ? ' ' + spanClassName : '')
+  // const finalInputClassName =
+  //   s.input +
+  //   (error ? ' ' + s.errorInput : ' ' + s.superInput) +
+  //   (className ? ' ' + s.className : '')
 
   return (
     <div className={s.inputWrapper}>
-      <span id={id ? id + '-span' : undefined} className={finalSpanClassName}>
-        {error}
-      </span>
-      <input
-        id={id}
-        type={'text'}
-        onChange={onChangeCallback}
-        onKeyPress={onKeyPressCallback}
-        className={finalInputClassName}
-        {...restProps}
-      />
+      <span>{error}</span>
+      <input type={'text'} {...restProps} />
     </div>
   )
 }
