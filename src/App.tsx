59aefd1e8bb1266688a9457928158abfb93971a1
yuliaslatvitskaya <yuli.slat@yandex.ru>
2023-08-01 12:01:34 +0200

    fix: App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -10,12 +10,31 @@ import { SignIn } from '../src/components/auth/signIn'
 import { LoginForm } from './components/auth/login-form/loginForm.tsx'
 import { SignUp } from './components/auth/signUp/signUp.tsx'
 import Layout from './components/ui/Layout/Layout.tsx'
-import Login from './pages/LoginPage/Login.tsx'
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<Layout />}>
-      <Route path={'/login'} element={<Login />} />
+      <Route path={'/login'} element={<LoginForm />} />
+      <Route
+        path={'/SignIn'}
+        element={
+          <SignIn
+            onSubmit={() => {
+              'test'
+            }}
+          />
+        }
+      />
+      <Route
+        path={'/SignUp'}
+        element={
+          <SignUp
+            onSubmit={() => {
+              'test'
+            }}
+          />
+        }
+      />
     </Route>
   )
 )
@@ -24,17 +43,6 @@ export function App() {
   return (
     <div>
       <RouterProvider router={router} />
-      <LoginForm />
-      <SignIn
-        onSubmit={() => {
-          'test'
-        }}
-      />
-      <SignUp
-        onSubmit={() => {
-          'test'
-        }}
-      />
     </div>
   )
 }
