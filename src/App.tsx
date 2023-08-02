4305a788b8d36322fe22f692cb2b59c1766eec45
yuliaslatvitskaya <yuli.slat@yandex.ru>
2023-08-02 14:42:41 +0200

    fix: fix bug after Ilya

--- src/App.tsx
+++ src/App.tsx
@@ -5,14 +5,12 @@ import {
   RouterProvider,
 } from 'react-router-dom'
 
-
 import { SignIn } from '../src/components/auth/signIn'
 
 import { CheckEmail } from './components/auth/checkEmail/checkEmail.tsx'
 import { ForgotPassword } from './components/auth/forgotPassword/forgotPassword.tsx'
 import { SignUp } from './components/auth/signUp/signUp.tsx'
-import Layout from './components/ui/Layout/Layout.tsx'
-
+import { Layout } from './components/ui/Layout/Layout.tsx'
 
 const router = createBrowserRouter(
   createRoutesFromElements(
--- src/components/auth/signUp/signUp.tsx
+++ src/components/auth/signUp/signUp.tsx
@@ -5,7 +5,7 @@ import { Link } from 'react-router-dom'
 import { z } from 'zod'
 
 import { Button, TextField } from '../../ui'
-import Card from '../../ui/card/Card.tsx'
+import { Card } from '../../ui/card/Card.tsx'
 
 import s from './signUp.module.scss'
 
