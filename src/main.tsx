14a007696cd97182b1bd88c7c182c6fc351ebac3
yuliaslatvitskaya <yuli.slat@yandex.ru>
2023-08-06 13:15:46 +0200

    fix: fix breaking bug

--- src/main.tsx
+++ src/main.tsx
@@ -4,11 +4,15 @@ import './styles/index.scss'
 import { StrictMode } from 'react'
 
 import { createRoot } from 'react-dom/client'
+import { Provider } from 'react-redux'
 
 import { App } from './App.tsx'
+import { store } from './services/store.ts'
 
 createRoot(document.getElementById('root') as HTMLElement).render(
   <StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </StrictMode>
 )
