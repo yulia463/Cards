70de570be7737cdf1ddb942a77ae999d8851f29d
Ilya Matsiushyn <matushin.97@gmail.com>
2023-08-24 15:32:29 +0300

    fix: add args to profile stories

--- src/components/ui/profile/Profile.stories.tsx
+++ src/components/ui/profile/Profile.stories.tsx
@@ -13,5 +13,7 @@ export default meta
 type Story = StoryObj<typeof meta>
 
 export const Primary: Story = {
-  args: {},
+  args: {
+    personalInfo: { name: 'string', email: 'string', profileImg: 'string' },
+  },
 }
