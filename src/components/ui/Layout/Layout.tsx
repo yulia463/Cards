213d2db1bb08dcabcbe20cd06546aa9f9ab04008
Ilya Matsiushyn <matushin.97@gmail.com>
2023-08-25 14:29:42 +0300

    fix: linting problems

--- src/components/ui/Layout/Layout.tsx
+++ src/components/ui/Layout/Layout.tsx
@@ -1,18 +1,19 @@
 import { Outlet } from 'react-router-dom'
 
+import { useGetDecksQuery, useGetUserDataQuery } from '../../../services/base-api'
 import { Header } from '../Header'
 
-import {useGetDecksQuery, useGetUserDataQuery} from "../../../services/base-api";
-
 export const Layout = () => {
-    // const result = useGetDecksQuery();
-    const userData = useGetUserDataQuery()
-    // console.log(result)
-    console.log(userData)
+  // const result = useGetDecksQuery();
+  const userData = useGetUserDataQuery()
+
+  // console.log(result)
+  console.log(userData)
+
   return (
     <main>
       <Header />
-        {/*<div>{result?.data?.items?.map(el => el.name)}</div>*/}
+      {/*<div>{result?.data?.items?.map(el => el.name)}</div>*/}
       <div
         style={{
           height: 'calc(100vh - 56px)',
