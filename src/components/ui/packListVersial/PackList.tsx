3bd0fcc86a83dd2b8ebf18251188a05b0f9d4271
yuliaslatvitskaya <yuli.slat@yandex.ru>
2023-09-07 17:37:21 +0200

    feat : add styles for PackList and add icon

--- src/components/ui/packListVersial/PackList.tsx
+++ src/components/ui/packListVersial/PackList.tsx
@@ -2,6 +2,9 @@ import { RangeSlider } from '../../ui/rangeSlider/RangeSlider.tsx'
 import { Table } from '../../ui/table/table.tsx'
 import { TextField } from '../../ui/textField/textField.tsx'
 import { Button } from '../button/button.tsx'
+import { SearchIcon } from '../icons/searchIcon.tsx'
+
+import s from './PaskList.module.scss'
 
 type PackListType = {
   nameForPack?: string
@@ -9,9 +12,13 @@ type PackListType = {
 export const PackList = (props: PackListType) => {
   return (
     <div>
-      <div>{props.nameForPack}</div>
-      <Button>Add New Pack</Button>
-      <TextField />
+      <div className={s.titleWrapper}>
+        <p className={s.nameForPack}>{props.nameForPack}</p>
+        <Button variant="primary">Add New Pack</Button>
+      </div>
+      <div className={s.input}>
+        <TextField placeholder={'input search'} icon={<SearchIcon />} />
+      </div>
       <div>
         <div>Show packs cards</div>
         <Button>My Cards</Button>
