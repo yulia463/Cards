bef2978219ac91891c19313683235b05a680d672
KarinaFomina04 <karinafomina.19970804@gmail.com>
2023-09-12 16:02:58 +0200

    refactor: FriendsPackList.tsx

--- src/components/ui/packListVersial/FriendsPackList.tsx
+++ src/components/ui/packListVersial/FriendsPackList.tsx
@@ -3,26 +3,30 @@ import { useState } from 'react'
 import { Table } from '../../ui/table/table.tsx'
 import { Button } from '../button/button.tsx'
 import { LeftArrowIcon } from '../icons/leftArrowIcon.tsx'
-import { SearchIcon } from '../icons/searchIcon.tsx'
 
 import s from './FriendsPackList.module.scss'
-import {TextField} from "src/components/ui";
+
+import { SearchInput } from 'src/components/ui/searchInput/searchInput.tsx'
+
+const testData = [
+  { id: 1, name: 'Lucas', cardsNumber: 4, lastDate: '24.07.2023', createdBy: 'Ivan Ivanov' },
+  { id: 2, name: 'Olivia', cardsNumber: 4, lastDate: '25.07.2023', createdBy: 'Ivan Ivanov' },
+  { id: 3, name: 'Liam', cardsNumber: 4, lastDate: '26.07.2023', createdBy: 'Ivan Ivanov' },
+  { id: 4, name: 'Emma', cardsNumber: 4, lastDate: '27.07.2023', createdBy: 'Ivan Ivanov' },
+  { id: 5, name: 'Noah', cardsNumber: 4, lastDate: '28.07.2023', createdBy: 'Ivan Ivanov' },
+  { id: 6, name: 'Ava', cardsNumber: 4, lastDate: '29.07.2023', createdBy: 'Ivan Ivanov' },
+  { id: 7, name: 'Isabella', cardsNumber: 4, lastDate: '30.07.2023', createdBy: 'Ivan Ivanov' },
+]
 
 export const FriendsPackList = () => {
-  const testData = [
-    { id: 1, name: 'Lucas', cardsNumber: 4, lastDate: '24.07.2023', createdBy: 'Ivan Ivanov' },
-    { id: 2, name: 'Olivia', cardsNumber: 4, lastDate: '25.07.2023', createdBy: 'Ivan Ivanov' },
-    { id: 3, name: 'Liam', cardsNumber: 4, lastDate: '26.07.2023', createdBy: 'Ivan Ivanov' },
-    { id: 4, name: 'Emma', cardsNumber: 4, lastDate: '27.07.2023', createdBy: 'Ivan Ivanov' },
-    { id: 5, name: 'Noah', cardsNumber: 4, lastDate: '28.07.2023', createdBy: 'Ivan Ivanov' },
-    { id: 6, name: 'Ava', cardsNumber: 4, lastDate: '29.07.2023', createdBy: 'Ivan Ivanov' },
-    { id: 7, name: 'Isabella', cardsNumber: 4, lastDate: '30.07.2023', createdBy: 'Ivan Ivanov' },
-  ]
-
   const [searched, setSearched] = useState<string>('')
   const filteredRows = testData.filter(row => {
     return row.name.toLowerCase().includes(searched.toLowerCase())
   })
+
+  const cancelSearch = () => {
+    setSearched('')
+  }
 
   return (
     <div className={s.container}>
@@ -34,10 +38,12 @@ export const FriendsPackList = () => {
         <p className={s.nameForPack}>Friend’s Pack</p>
         <Button variant="primary">Learn to Pack</Button>
       </div>
-      <TextField
-        style={{ marginBottom: '20px' }}
-        placeholder={'input search'}
-        icon={<SearchIcon />}
+      <SearchInput
+        value={searched}
+        onChange={e => {
+          setSearched(e.target.value)
+        }}
+        onCancelSearch={() => cancelSearch()}
       />
       <Table rows={filteredRows} />
     </div>
