8799b812e5ce3dd80f6d7067c2e8f8e51356b3ba
yuliaslatvitskaya <yuli.slat@yandex.ru>
2023-09-28 15:19:26 +0200

    feat: add Error component

--- src/components/ui/error/error.tsx
+++ src/components/ui/error/error.tsx
@@ -1,10 +1,13 @@
 import {PageNotFoundIcon} from "src/components/ui/icons/pageNotFoundIcon.tsx";
 import {Button} from "src/components/ui";
 import s from './error.module.scss'
+import {Link} from "react-router-dom";
 export const Error = () => {
     return <div className={s.container}>
         <PageNotFoundIcon/>
         <div className={s.text}>Sorry! Page not found!</div>
+        <Link to={'/myPack'}>
         <Button>Back to home page</Button>
+        </Link>
     </div>
 }
